e3d163f7a60e364119ecc67e2bdea312a568f67e
user <user@LAPTOP-MO3LOTSK>
2020-02-17 15:40:07 -0500

    second

--- src/javaapplication92/JavaApplication92.java
+++ src/javaapplication92/JavaApplication92.java
@@ -17,6 +17,7 @@ public class JavaApplication92 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world! ");
+        System.out.println("my name is meet");
     }
     
 }
